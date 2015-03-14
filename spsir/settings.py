80d438f59ef668a66cd7ed0bc992da0501d0a4ae
rahul-satal <rahulsatal92@gmail.com>
2015-03-14 10:21:43 +0530

    All settings done for django

--- spsir/settings.py
+++ spsir/settings.py
@@ -85,4 +85,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
+STATICFILES_DIR = (
+    ('assets', '/home/rahul/mydjangoapp/spsir/spsir_website/static'),
+    )
